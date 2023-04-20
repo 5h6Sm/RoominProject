14b38f6e5a1634757d818ed4afd3e0ed0d6141d5
youjin <w2117@e-mirim.hs.kr>
2023-04-20 15:19:46 +0900

    TimePicker

--- app/src/main/java/com/example/roomins/ChoiceCall.kt
+++ app/src/main/java/com/example/roomins/ChoiceCall.kt
@@ -6,15 +6,15 @@ import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.TimePicker
+import android.view.View
+import android.widget.*
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.text.set
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.util.*
+import kotlin.math.min
 
 class ChoiceCall : AppCompatActivity() {
     private lateinit var timePicker: TimePicker
@@ -32,7 +32,70 @@ class ChoiceCall : AppCompatActivity() {
             bottomSheetDialog.setContentView(bottomSheetView)
             // bottomSheetDialog 호출
             bottomSheetDialog.show()
-            timePicker = bottomSheetView.findViewById<TimePicker>(R.id.timePicker)
+
+            val day : NumberPicker = bottomSheetView.findViewById(R.id.day_datepicker)
+            val hour : NumberPicker = bottomSheetView.findViewById(R.id.hour_datepicker)
+            val minute : NumberPicker = bottomSheetView.findViewById(R.id.minute_datepicker)
+
+            //  순환 안되게 막기
+            day.wrapSelectorWheel = false
+            hour.wrapSelectorWheel = false
+            minute.wrapSelectorWheel = false
+
+            //  editText 설정 해제
+            day.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            hour.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            minute.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+
+            val calendar = Calendar.getInstance()
+            val today = calendar.get(Calendar.DAY_OF_WEEK)
+            val days = calendar.get(Calendar.DAY_OF_MONTH)
+            val month = calendar.get(Calendar.MONTH) + 1
+
+            //  최소값 설정
+            day.minValue = days
+            hour.minValue = 1
+            minute.minValue = 1
+
+            //  최대값 설정
+            day.maxValue = 31
+            hour.maxValue = 23
+            minute.maxValue = 59
+
+            val dayOfWeek = when(today) {
+                Calendar.SUNDAY -> "일요일"
+                Calendar.MONDAY -> "월요일"
+                Calendar.TUESDAY -> "화요일"
+                Calendar.WEDNESDAY -> "수요일"
+                Calendar.THURSDAY -> "목요일"
+                Calendar.FRIDAY -> "금요일"
+                Calendar.SATURDAY -> "토요일"
+                else -> ""
+            }
+
+            //보여질 값 설정
+            val dayDisplayedValues = mutableListOf<String>()
+            dayDisplayedValues.add("오늘($dayOfWeek)")
+            val tomorrowDayOfWeek = when((today+1)%7) {
+                Calendar.SUNDAY -> "일요일"
+                Calendar.MONDAY -> "월요일"
+                Calendar.TUESDAY -> "화요일"
+                Calendar.WEDNESDAY -> "수요일"
+                Calendar.THURSDAY -> "목요일"
+                Calendar.FRIDAY -> "금요일"
+                Calendar.SATURDAY -> "토요일"
+                else -> ""
+            }
+            dayDisplayedValues.add("$month/$days($tomorrowDayOfWeek)")
+            day.setDisplayedValues(dayDisplayedValues.toTypedArray())
+
+            hour.setDisplayedValues(arrayOf("09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"))
+
+            val minuteDisplayedValues = mutableListOf<String>()
+            for (i in 30..59) {
+                minuteDisplayedValues.add(i.toString())
+            }
+            minute.setDisplayedValues(minuteDisplayedValues.toTypedArray())
 
             val text_edit_noice = bottomSheetView.findViewById<TextView>(R.id.text_edit_noice);
 
