11dafff2b40e4708fdd8792799fd1a4cab2a403f
5h6Sm <5h6sm.github@gmail.com>
2023-04-28 13:55:07 +0900

    registered_hotel_map.xml frame layout 수정중

--- app/src/main/java/com/example/roomins/RegisteredHotelMap.kt
+++ app/src/main/java/com/example/roomins/RegisteredHotelMap.kt
@@ -2,6 +2,8 @@ package com.example.roomins
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.roomins.R.id.textView6
 
@@ -9,8 +11,9 @@ class RegisteredHotelMap : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registered_hotel_map)
-        val t = findViewById<TextView>(R.id.textView6);
-        t.bringToFront();
+        val t = findViewById<LinearLayout>(R.id.br_front)
+        val parentLayout = t.parent as ViewGroup
+        parentLayout.bringChildToFront(t)
     }
 }
     
