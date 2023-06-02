aea9aaee39809ac1df8d9e007059c40be550ef23
5h6sm <5h6sm.github@gmail.com>
2023-06-02 14:25:28 +0900

    지갑 수정

--- app/src/main/java/com/example/roomins/IdLogin.kt
+++ app/src/main/java/com/example/roomins/IdLogin.kt
@@ -1,11 +1,21 @@
 package com.example.roomins
 
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class IdLogin : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_id_login)
+
+        val imageView = findViewById<ImageView>(R.id.arrow_matrix)
+        val originalBitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val flippedBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, originalBitmap.width, originalBitmap.height, Matrix().apply { postScale(-1f, 1f) }, true)
+        imageView.setImageBitmap(flippedBitmap)
     }
 }
