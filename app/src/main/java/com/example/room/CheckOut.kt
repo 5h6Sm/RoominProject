89e5a5d0e703a96d4041397c824a7f09315b7625
doyeonHa <w2113@e-mirim.hs.kr>
2023-06-19 22:36:58 +0900

    스위치 기능 구현 외 다수..

--- app/src/main/java/com/example/room/CheckOut.kt
+++ app/src/main/java/com/example/room/CheckOut.kt
@@ -1,4 +1,40 @@
 package com.example.room
 
-class CheckOut {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.fragment.app.Fragment
+import com.example.roomins.R
+
+class CheckOut : Fragment() {
+    private lateinit var checkInRadioButton: RadioButton
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_checkout, container, false)
+
+        val radioGroup = view.findViewById<RadioGroup>(R.id.toggle)
+        checkInRadioButton = view.findViewById<RadioButton>(R.id.search)
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.search -> {
+                    navigateToCheckInFragment()
+                }
+            }
+        }
+
+        return view
+    }
+
+    private fun navigateToCheckInFragment() {
+        val fragmentManager = requireActivity().supportFragmentManager
+        fragmentManager.popBackStack()
+    }
 }
