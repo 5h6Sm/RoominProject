ad81afa8277a1c69428bca8b6517c42f618aa5f4
5h6sm <5h6sm.github@gmail.com>
2023-06-20 16:16:32 +0900

    QR 프래그먼트 수정 중

--- app/src/main/java/com/example/roomins/HomeActivity.kt
+++ app/src/main/java/com/example/roomins/HomeActivity.kt
@@ -1,34 +1,57 @@
 package com.example.roomins
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import com.google.zxing.qrcode.QRCodeWriter
 
+fun generateQRCode(text: String, width: Int, height: Int): Bitmap? {
+    try {
+        val bitMatrix: BitMatrix = QRCodeWriter().encode(
+            text,
+            BarcodeFormat.QR_CODE,
+            width,
+            height
+        )
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        for (x in 0 until width) {
+            for (y in 0 until height) {
+                bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+            }
+        }
+        return bitmap
+    } catch (e: WriterException) {
+        e.printStackTrace()
+    }
+    return null
+}
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var showFragmentButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        if (savedInstanceState == null) {
-            showHomeFragment()
-        }
+        val qrCodeText = "내용을 포함시킬 QR 코드 정보"
+        val qrCodeWidth = 500
+        val qrCodeHeight = 500
 
-        showFragmentButton = findViewById(R.id.button6)
-        showFragmentButton.setOnClickListener {
-            showHomeFragment()
-        }
+        val qrCodeImageView: ImageView = findViewById(R.id.qr_back)
+        val qrCodeBitmap = generateQRCode(qrCodeText, qrCodeWidth, qrCodeHeight)
+        qrCodeImageView.setImageBitmap(qrCodeBitmap)
+
+        qrCodeImageView.visibility = View.GONE
+
+//        var button
     }
 
-    private fun showHomeFragment() {
-        val homeFragment = HomeFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, homeFragment)
-            .addToBackStack(null)
-            .commit()
-    }
+
 }
--- app/src/main/java/com/example/roomins/HomeFragment.kt
+++ app/src/main/java/com/example/roomins/HomeFragment.kt
@@ -1,48 +1,47 @@
 package com.example.roomins
 
-import android.graphics.Bitmap
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.MultiFormatWriter
-import com.google.zxing.WriterException
-import com.google.zxing.common.BitMatrix
-import com.google.zxing.qrcode.QRCodeWriter
 
-fun generateQRCode(text: String, width: Int, height: Int): Bitmap? {
-    try {
-        val bitMatrix: BitMatrix = QRCodeWriter().encode(
-            text,
-            BarcodeFormat.QR_CODE,
-            width,
-            height
-        )
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
-            }
-        }
-        return bitmap
-    } catch (e: WriterException) {
-        e.printStackTrace()
-    }
-    return null
-}
-
+//import android.graphics.Bitmap
+//import android.graphics.Color
+//import android.os.Bundle
+//import android.view.View
+//import android.widget.ImageView
+//import androidx.fragment.app.Fragment
+//import com.google.zxing.BarcodeFormat
+//import com.google.zxing.MultiFormatWriter
+//import com.google.zxing.WriterException
+//import com.google.zxing.common.BitMatrix
+//import com.google.zxing.qrcode.QRCodeWriter
+//
+//fun generateQRCode(text: String, width: Int, height: Int): Bitmap? {
+//    try {
+//        val bitMatrix: BitMatrix = QRCodeWriter().encode(
+//            text,
+//            BarcodeFormat.QR_CODE,
+//            width,
+//            height
+//        )
+//        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+//        for (x in 0 until width) {
+//            for (y in 0 until height) {
+//                bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+//            }
+//        }
+//        return bitmap
+//    } catch (e: WriterException) {
+//        e.printStackTrace()
+//    }
+//    return null
+//}
+//
 class HomeFragment : Fragment(R.layout.fragment_home){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val qrCodeText = "내용을 포함시킬 QR 코드 정보"
-        val qrCodeWidth = 500
-        val qrCodeHeight = 500
 
-        val qrCodeImageView: ImageView = view.findViewById(R.id.qr_back)
-        val qrCodeBitmap = generateQRCode(qrCodeText, qrCodeWidth, qrCodeHeight)
-        qrCodeImageView.setImageBitmap(qrCodeBitmap)
     }
 }
 
