3d6298f47d04bd506952535b0e56fecac24dc4a7
5h6sm <5h6sm.github@gmail.com>
2023-06-20 15:21:11 +0900

    QR 프래그먼트 띄우기

--- app/src/main/java/com/example/roomins/HomeActivity.kt
+++ app/src/main/java/com/example/roomins/HomeActivity.kt
@@ -3,16 +3,32 @@ package com.example.roomins
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.Button
+import android.widget.ImageButton
+
+
 class HomeActivity : AppCompatActivity() {
+    private lateinit var showFragmentButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, HomeFragment())
-                .commit()
+            showHomeFragment()
+        }
+
+        showFragmentButton = findViewById(R.id.button6)
+        showFragmentButton.setOnClickListener {
+            showHomeFragment()
         }
     }
-}
 
+    private fun showHomeFragment() {
+        val homeFragment = HomeFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, homeFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
